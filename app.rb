7453d72508af80f3022af335aa0f2f0c083f700c
olvap <oldani.pablo@gmail.com>
2015-09-08 21:38:47 -0300

    Initial commit.

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'cuba'
+require 'redic'
+require 'pry'
+
+redis = Redic.new
+
+Cuba.define do
+  on get do
+    locations = redis.call 'LRANGE', 'locations', 0, -1
+    res.write locations
+  end
+
+  on post do
+    redis.call 'LPUSH', 'locations', req.body.read
+    res.status = 201
+  end
+end
